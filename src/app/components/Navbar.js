4bb5c122da454bd41426fa5f1080547ff87b1a94
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2025-03-13 01:39:23 +0600

    navbar added

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
-    return (
-        <nav>
-           <ul className='flex items-center justify-center gap-5'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Service</li>
-            </ul> 
-        </nav>
-    );
+  return (
+    <nav className="flex items-center justify-between w-full max-w-7xl mx-auto">
+      <div>
+        <h3>Logo</h3>
+      </div>
+      <div>
+        <ul className="flex items-center justify-center gap-5">
+          <li>Home</li>
+          <li>My Resume</li>
+          <li>Templates</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+      <div>
+        <button>Sign In</button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
