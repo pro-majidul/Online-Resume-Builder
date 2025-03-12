75dae4c73cf49e72de6c0e8f0cb629ebdb12ed8a
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2025-03-13 01:47:34 +0600

    resolve murge added

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="w-full max-w-7xl mx-auto justify-items-center place-content-center h-screen">
       <h3> Hello Developers</h3>
+      <p>Please clone developers branch and push your own branch </p>
     </div>
   );
 }
